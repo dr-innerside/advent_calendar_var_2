3cb995a35586a9cf666d734271257ff33042e90b
migdracios <migdracios@gmail.com>
2023-11-15 20:18:06 +0900

    Fix 모달 열리지 않는 버그

--- index.js
+++ index.js
@@ -120,12 +120,13 @@ doors.forEach((door, index) => {
     door.addEventListener('click', () => handleClick(index));
 });
 
+// 목표 날짜 설정 (예시: 2023년 1월 1일)
+const targetDate = new Date("2023-10-25T00:00:00Z");
+
 function updateCountdown() {
     // 현재 한국 시간을 얻어오기
     const now = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Seoul" }));
 
-    // 목표 날짜 설정 (예시: 2023년 1월 1일)
-    const targetDate = new Date("2023-12-25T00:00:00Z");
 
     // 남은 시간 계산
     const timeRemaining = targetDate - now;
@@ -147,12 +148,30 @@ function handleClick(index) {
     const now = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Seoul" }));
 
     // 각 날짜에 해당하는 날짜를 계산
-    const targetDate = new Date(2023, 11, index + 1); // 2023년 12월 1일부터 시작
+    // const targetDate = new Date(2023, 11, index + 1); // 2023년 12월 1일부터 시작
 
     // 현재 날짜가 목표 날짜 이후인지 확인
     if (now.getTime() > targetDate.getTime()) {
         // 여기에 모달을 열거나 특정 동작을 수행하는 코드 추가
-        alert('이벤트 캘린더를 엽니다.');
+
+         // 상위 div의 class 번호를 찾아서 image url에 사용합니다
+         
+         
+         const imageUrl = `image/card/card-${index+1}.png`;
+ 
+         // 'back' 클래스를 가진 요소를 찾아 스타일을 가져옵니다.
+         const doorDiv = document.querySelector(`.day-${index+1}`)
+         const backDiv = doorDiv.querySelector(`.back`);
+ 
+         const style = window.getComputedStyle(backDiv);
+         const pTag = backDiv.querySelector('p')
+         const text = modalMessageList[index]
+ 
+         // const backgroundImageUrl = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+ 
+         // showModal 함수를 호출하여 모달을 표시합니다.
+         showModal(imageUrl, text);
+        // alert('이벤트 캘린더를 엽니다.');
 
         // CSS 적용
         applyCss(index);
